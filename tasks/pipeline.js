613010558971a061d16e36fc854e36018f9c26cd
eduaro <Eduar.Yesid.Rodriguez.Caro@Elion.ee>
2015-07-25 19:26:14 +0300

    add bowerrc to change  bower components path

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -31,7 +31,8 @@ var jsFilesToInject = [
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
-  'js/**/*.min.js'
+  'js/**/*.min.js',
+  'bower_components/**/*.min.js'
 ];
 
 
