8ab815ff9a197bf4135ea9658d5007877c352497
eduaro <Eduar.Yesid.Rodriguez.Caro@Elion.ee>
2015-07-25 18:48:55 +0300

    Created world entitiy

---
+++ api/models/World.js
@@ -0,0 +1,26 @@
+/**
+* World.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    rules:{
+      type: 'string'
+    },
+    master:{
+      model: 'user'
+    },
+    board :{
+      collection: 'board',
+      via: 'ofGuild'
+    },
+    moderators :{
+      collection: 'user'
+    }
+  }
+};
+
