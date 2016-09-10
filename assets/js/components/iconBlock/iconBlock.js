77fb5009e8991b634b89a815f602ecfb6e433fd6
jevgenir <zeka.rum@gmail.com>
2016-09-10 15:18:13 +0300

    car details still...

---
+++ assets/js/components/iconBlock/iconBlock.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jevgenir on 10.09.2016.
+ */
+
+angular.module('application').component('iconBlock', {
+  templateUrl: '/js/components/iconBlock/iconBlock.html',
+  bindings: {
+    text: '<',
+    icon: '<'
+  }
+});
