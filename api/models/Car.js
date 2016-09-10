f834a2dcc0a0f7da3132c48878182118ecc88b3f
eduaro <Eduar.Yesid.Rodriguez.Caro@Elion.ee>
2016-09-10 13:04:00 +0300

    add postgres model fields

--- api/models/Car.js
+++ api/models/Car.js
@@ -9,7 +9,35 @@ module.exports = {
   identity: 'car',
   connection: 'skyndaPostgresqlServer',
   attributes: {
-    brand:'string'
+    id: {
+      type: 'text',
+      defaultsTo: function () {
+        return uuid.v4();
+      }
+    },
+    created: 'datetime',
+    VIN: 'string',
+    Price: 'string',
+    Transmission: 'string',
+    Mileage: 'string',
+    InitialReg: 'string',
+    RegistrationNumber: 'string',
+    Engine: 'string',
+    HorsePower: 'string',
+    Drive: 'string',
+    Doors: 'string',
+    Seats: 'string',
+    Brand: 'string',
+    Model: 'string',
+    Color: 'string',
+    BodyType: 'string',
+    CityFuel: 'string',
+    HighWayFuel: 'string',
+    images: 'text',
+    IsSold: 'boolean',
+    DescriptionBrand: 'text',
+    DescriptionModel: 'text',
+    Archived: 'datetime'
   }
 };
 
